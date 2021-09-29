846d0fea35dcc9ea3dbec974e4c887bfe39d28d7
simonbandmann <s.bandmann@gmail.com>
2021-09-29 17:25:55 +0200

    MUI integration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>This is a test </p>
+  return (      
+    <Container maxWidth="lg" className="App-header">
 
+  <Grid container spacing={2}>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+</Grid>
 
-
-        <p> This is another paragraph.</p>
-      </header>
-    </div>
+    </Container>
   );
 }
 
